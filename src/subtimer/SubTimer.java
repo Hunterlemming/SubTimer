3466ae0a66bee6e777416d0459158247952db550
krisztian <krisztian@krisztian-Lenovo-Y520-15IKBN>
2018-08-10 13:45:23 +0200

    -- SUMMARY --
    Initial commit

---
+++ src/subtimer/SubTimer.java
@@ -0,0 +1,14 @@
+package subtimer;
+
+public class SubTimer {
+    
+    public static void main(String[] args) {
+        SubTimer ST = new SubTimer();
+        ST.start();
+    }
+    
+    public void start(){
+        
+    }
+    
+}
